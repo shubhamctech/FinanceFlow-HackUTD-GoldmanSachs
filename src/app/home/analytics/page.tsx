09452842a4eb9050ed2ba1a0405ad5ba79b4b43e
TheKillerKetchup <48397623+TheKillerKetchup@users.noreply.github.com>
2024-11-16 16:12:18 -0600

    sads

---
+++ src/app/home/analytics/page.tsx
@@ -0,0 +1,12 @@
+// Example: In another component or page
+import Analytics from '@/components/analytics';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Analytics />
+    </div>
+  );
+};
+
+export default Dashboard;
